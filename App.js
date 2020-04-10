c8ca18e7d7816feffb478fdf70a22bebddc126cc
AndrewsDutraPgMdR <dutrapgmdr@gmail.com>
2020-04-09 23:03:36 -0300

    Style

--- App.js
+++ App.js
@@ -1,36 +1,32 @@
 import React, {Component} from 'react';
-import {View,Text, Button} from 'react-native';
+import {View,Text, StyleSheet} from 'react-native';
 
 class App extends Component{
-  constructor (props){
-    super(props)
-    this.state = {
-      nome: "Andrews"
-    };
-    this.entrar = this.entrar.bind(this);
-  }
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
   render(){
-
-    let nome = 'Andrews';
-    
-
-
     return(
-      <View style={{marginTop: 50}}>
-        <Button title="Entrar" onPress={() => this.entrar('Algum texto')}/>
-        <Text style={{fontSize:23,color:'red',textAlign:'center'}}>
-           {this.state.nome}
-        </Text>
+      <View style={styles.area}>
+        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}> Eu sou texto 1</Text>
+        <Text style={styles.alinhaTexto}> Eu sou texto 2</Text>
+        <Text> Eu sou texto 3</Text>
+        <Text style={styles.textoPrincipal}> Eu sou texto 4</Text>
         
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  area:{
+    marginTop: 40
+    
+  },
+  textoPrincipal:{
+    fontSize: 25,
+    color:'red'
+  },
+  alinhaTexto:{
+    textAlign: 'center'
+  }
+});
 
 
 
